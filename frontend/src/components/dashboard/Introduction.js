3f2e908cf5d7e8a578b8b193b81ff2ed131b1651
Hanyun1 <hanyzhang@student.unimelb.edu.au>
2022-10-22 08:34:18 +0800

    introduction page

--- frontend/src/components/dashboard/Introduction.js
+++ frontend/src/components/dashboard/Introduction.js
@@ -61,7 +61,7 @@ const Introduction = () => {
                     <ContentTitle>Overview:</ContentTitle>
                     <ContentTxt>Melbourne is the coastal capital of the southeastern Australian state of Victoria. At the city's centre is the modern Federation Square development, with plazas, bars, and restaurants by the Yarra River. In the Southbank area, the Melbourne Arts Precinct is the site of Arts Centre Melbourne – a performing arts complex – and the National Gallery of Victoria, with Australian and indigenous art.</ContentTxt>  
                 
-                    <TagCloud 
+                    {/* <TagCloud 
                     style={{
                     fontFamily: 'sans-serif',
                     fontSize: 30,
@@ -74,7 +74,7 @@ const Introduction = () => {
                     <div style={{color: 'green'}}>tag</div>
                     <div rotate={90}>cloud</div>
                     ...
-                </TagCloud>
+                </TagCloud> */}
                 </Content>
             </div>
         </div>
